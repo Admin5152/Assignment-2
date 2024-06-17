b084bc7b6ed9e9a8134ae642c00dd720b869a2bb
@M <156658115+Admin5152@users.noreply.github.com>
2024-06-17 11:22:19 +0000

    marks developed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-g
-  std::cout << "Enter your age: ";
-  std::cin >> age;
-  std::cout << "Enter your marks: ";
-  std::cin >> marks;
-  std::cout << "Result: " << age * marks << std::endl;
+g std::cout << "Enter your age: ";
+std::cin >> age;
+std::cout << "Enter your real marks here: ";
+std::cin >> marks;
+std::cout << "Result: " << age * marks << std::endl;
 }
-
